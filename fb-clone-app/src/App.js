8893d57475106561be9a594314da5a6d2875a5be
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-12 23:58:54 +0530

    added signup page

--- fb-clone-app/src/App.js
+++ fb-clone-app/src/App.js
@@ -3,10 +3,28 @@ import Header from './components/Header';
 import SidebarLeft from './components/SidebarLeft';
 import Feed from './components/Feed';
 import SidebarRight from "./components/SidebarRight"
+import SignUp from './components/SignUp';
 
 function App() {
+
+  const user = null;
   return (
-    <></>
+    <>
+        {!user? (<SignUp />): (<div className="App">
+      <Header />
+      <div className="body">
+        {/* sidebar-left */}
+      <SidebarLeft />
+
+        {/* main feed  */}
+        <Feed />
+
+        {/* sidebar right  */}
+        <SidebarRight />
+
+      </div>
+    </div>)}
+    </>
   );
 }
 
