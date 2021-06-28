006e08d3fe4253741448635295a91813b31858e1
vidhi-thakur <72219676+vidhi-thakur@users.noreply.github.com>
2021-06-28 18:31:33 +0530

    updated post.js file

--- fb-clone-app/src/components/Post.js
+++ fb-clone-app/src/components/Post.js
@@ -19,6 +19,7 @@ function Post({porfileImg, img, title, input, timestamp}) {
                 <p className="mar ip">{input}</p>
                 {img && <img className="ip_img" src={img} />}
             </div>
+            
             <div className="post__icons">
                 <div className="icons--lc">
                     <ThumbUpAltRoundedIcon className="icon--bottom" />
